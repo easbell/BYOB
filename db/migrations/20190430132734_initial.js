8819c74427b3dc2375ae992dcdbc3a38072dc753
Elizabeth Asbell <e.asbell8@gmail.com>
2019-04-30 13:33:33 -0600

    Set up knex migrations.

---
+++ db/migrations/20190430132734_initial.js
@@ -0,0 +1,31 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('parties', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('symbol');
+      table.string('founded');
+    }),
+
+    knex.schema.createTable('tweets', function(table) {
+      table.increments('id').primary();
+      table.string('username');
+      table.string('content');
+      table.string('date_deleted');
+      table.integer('parties_id').unsigned();
+      table.foreign('parties_id')
+        .references('parties.id');
+
+      table.timestamp(true, true);
+    })
+
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('tweets'),
+    knex.schema.dropTable('parties')
+  ]);
+};
