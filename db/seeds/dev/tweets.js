595ce2fd672342f91952ecdb5beb09ed63b08c83
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-01 09:26:29 -0600

    Finish seeding data.

--- db/seeds/dev/tweets.js
+++ db/seeds/dev/tweets.js
@@ -1,11 +1,33 @@
-const parties = require('../../politicalTweetData');
+const { parties, tweets } = require('../../politicalTweetData');
 
-const createTweet = (knex, party) => {
+const createParty = (knex, party) => {
   return knex('parties').insert({
     name: party.name,
     symbol: party.symbol,
     founded: party.founded
   }, 'id')
+    .then(partyId => {
+      let tweetPromises = [];
+
+      tweets.forEach(tweet => {
+        if(tweet.party === party.name) {
+          tweetPromises.push(
+            createTweet(knex, {
+              username: tweet.username,
+              content: tweet.content,
+              date_deleted: tweet.date_deleted,
+              parties_id: partyId[0]
+            })
+          )
+        } 
+      });
+
+      return Promise.all(tweetPromises);
+    })
+};
+
+const createTweet = (knex, tweet) => {
+  return knex('tweets').insert(tweet)
 }
 
 exports.seed = (knex, Promise) => {
@@ -15,7 +37,7 @@ exports.seed = (knex, Promise) => {
       let partyPromises = [];
 
       parties.forEach(party => {
-        partyPromises.push(createTweet(knex, party));
+        partyPromises.push(createParty(knex, party));
       });
 
       return Promise.all(partyPromises);
