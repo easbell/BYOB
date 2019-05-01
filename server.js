cdf5780e914d727f6ff65620cacfff332a9bd2f3
Elizabeth Asbell <e.asbell8@gmail.com>
2019-04-30 18:52:03 -0600

    Set up server.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () => console.log(`example app listening on ${port}`))
+
+app.get('/api/v1/tweets', (req, res) => {
+  database('tweets').select()
+    .then((tweets) => {
+      res.status(200).json(tweets);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
