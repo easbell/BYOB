44561c088b98e8c6f68e8c7bc8e7e56d54e3ca56
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-03 09:43:21 -0600

    Config port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 const app = express();
-const port = 3000;
+app.set('port', process.env.PORT || 3000)
 app.use(express.json());
 
 app.listen(port, () => console.log(`example app listening on ${port}`))
